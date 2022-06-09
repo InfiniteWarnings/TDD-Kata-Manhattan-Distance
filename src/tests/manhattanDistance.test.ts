d2b5a937955ed78958cf7a6809db1ceb2a757038
InfiniteWarnings <50310477+InfiniteWarnings@users.noreply.github.com>
2022-06-09 21:54:48 +0300

    Add: - failing manhattanDistance(point,point) test

--- src/tests/manhattanDistance.test.ts
+++ src/tests/manhattanDistance.test.ts
@@ -1,8 +1,15 @@
 import { manhattanDistance } from "../manhattanDistance";
+import Point from "../Point";
 
 describe("manhattanDistance()", () => {
   it("Returns an absolute int", () => {
     const distance = manhattanDistance();
     expect(distance).toBeGreaterThanOrEqual(0);
+  });
+
+  it("Gets the Manhattan distance between two Points", () => {
+    const pointA = new Point(6, 2);
+    const pointB = new Point(2, 6);
+    expect(manhattanDistance(pointA, pointB)).toBe(8);
   });
 });
