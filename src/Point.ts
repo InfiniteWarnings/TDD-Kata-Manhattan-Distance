63e96a6113b5ee36a8786d32f2f46b3f173af717
InfiniteWarnings <50310477+InfiniteWarnings@users.noreply.github.com>
2022-06-09 21:54:47 +0300

    Add: - Point constructor - private x,y members

--- src/Point.ts
+++ src/Point.ts
@@ -1,1 +1,17 @@
-export default class Point {}
+/**
+ * SPEC:
+ * - The class Point is immutable (its state cannot be changed after instantiation)
+ * - The class Point has no Getters
+ * - The class Point has no public properties (i.e. the internal state cannot be read from outside the class).
+ * - The class Point exists on a grid (X,Y coordinates)
+ */
+
+export default class Point {
+  private x: Number;
+  private y: Number;
+
+  constructor(x: Number, y: Number) {
+    this.x = x;
+    this.y = y;
+  }
+}
--- src/tests/Point.test.ts
+++ src/tests/Point.test.ts
@@ -2,7 +2,7 @@ import Point from "../Point";
 
 describe("Point", () => {
   it("is an instance of Point", () => {
-    const point = new Point();
+    const point = new Point(0, 0);
     expect(point).toBeInstanceOf(Point);
   });
 });
